0892a209635c11999f916376c7ed5273d8dbe029
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-11-15 18:07:09 +0100

    Update script.py via DeepSeek

--- script.py
+++ script.py
@@ -1,12 +1,8 @@
 import ml_training
 
-# Run the machine learning training
 print("=" * 80)
 print("RUNNING MACHINE LEARNING TRAINING ON CHARACTER DATASET")
 print("=" * 80)
 
-ml_training.main()
-
-print("\n" + "=" * 80)
-print("MACHINE LEARNING TRAINING COMPLETED")
-print("=" * 80)
+# Run the machine learning training
+ml_training.train_em_dash_detector()
